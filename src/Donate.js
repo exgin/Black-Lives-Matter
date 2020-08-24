66d0b9341a888c0bccf15a88545950d1f0acfa74
Samuel Reid <exgin3538@yahoo.com>
2020-08-24 19:25:32 -0400

    change up Donate scss to resemble Vote scss

--- src/Donate.js
+++ src/Donate.js
@@ -3,10 +3,10 @@ import './Donate.scss';
 
 function Donate() {
   return (
-    <div className='contianer vh-100 text-center'>
+    <div className='text-center'>
       <h1>DONATE</h1>
 
-      <div className='container'>
+      <div className='Donate vh-100'>
         <h3 className='Donate-title'>Funds & Organizations</h3>
         <ul className='funds-org list-unstyled'>
           <li>
@@ -35,17 +35,17 @@ function Donate() {
             </a>
           </li>
         </ul>
-      </div>
 
-      <div className='small-text'>
-        <small>
-          ...<b>only that many?</b> Why?
-        </small>
-      </div>
+        <div className='small-text'>
+          <small>
+            ...<b>only that many?</b> Why?
+          </small>
+        </div>
 
-      <div className='container'>
-        While donating to organizations and fundraisers are an excellent ways to boost awareness to a cause; unfortunately there <b>are many scams & shady practices occuring.</b> The best way to
-        donate is to support & give back your <b> local black / any minority businesses or community members.</b>
+        <div className='container'>
+          While donating to organizations and fundraisers are an excellent ways to boost awareness to a cause; unfortunately there <b>are many scams & shady practices occuring.</b> The best way to
+          donate is to support & give back your <b> local black / any minority businesses or community members.</b>
+        </div>
       </div>
     </div>
   );
